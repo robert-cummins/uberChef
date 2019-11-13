31012b6c1f8c78408b4dda812e5469baee2c7ef0
Student <student@eda-bc-06.localdomain>
2019-11-14 10:21:18 +1300

    Add getChefBycuisineAndLocation

--- dbFunctions.js
+++ dbFunctions.js
@@ -30,6 +30,18 @@ function getChefsByCuisine(cuisineId, db = database){
     .then(data => {console.log(data)})
 }
 
+function getChefsByCuisineAndLocation(cuisineId, location, db = database){
+    const baseQuery = db('cuisine')
+    .leftJoin('chefCuisine', 'chefCuisine.cuisine_id', 'cuisine.cuisine_id')
+    .leftJoin('chefs', "chefCuisine.chef_id", 'chefs.chef_id')
+
+    if(cuisineId) baseQuery.where('cuisine.cuisine_id', cuisineId)
+
+    if(location) baseQuery.where('chefs.location', location)
+
+    return baseQuery.then(data => {console.log(data)})
+}
+
 function getChefCuisines(chefId, db = database){
     return db('chefs')
     .leftJoin('chefCuisine', 'chefCuisine.chef_id', 'chefs.chef_id')
@@ -47,5 +59,6 @@ module.exports = {
     getChefsByLocation,
     getChefsByCuisine,
     getChefCuisines,
-    getChefById
+    getChefById,
+    getChefsByCuisineAndLocation
 }
