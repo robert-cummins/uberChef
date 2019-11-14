19d965f23b3021f847457f919dab5725a84ce733
Student <student@eda-bc-06.localdomain>
2019-11-14 16:15:47 +1300

    update

--- public/app.js
+++ public/app.js
@@ -1,9 +1,33 @@
 let citySelect = document.getElementById('city-select')
 let mainSubmit = document.getElementById('main-submit')
 let mainForm = document.getElementById('main-form')
+let cuisineForm = document.getElementById('cuisine-form')
+let cuisineSelect = document.getElementById('cuisine-select')
+let cuisineSubmit = document.getElementById('cuisine-submit')
+let selectedCity
+let url = window.location.href
 
-mainSubmit.addEventListener('click', () => {
-    let selectedCity = citySelect.value
-    console.log(selectedCity)
-    mainForm.action = selectedCity
+function getImageDirectoryByFullURL(url){
+    return url.split('/').pop()
+}
+
+if(mainSubmit){
+    mainSubmit.addEventListener('click', () => {
+        
+        let selectedCity = citySelect.value
+        console.log(selectedCity)
+        mainForm.action = selectedCity
+    })
+}
+selectedCity = getImageDirectoryByFullURL(url)
+console.log(selectedCity)
+
+
+cuisineSubmit.addEventListener('click', () =>{
+    
+    console.log(url)
+    let selectedCuisine = cuisineSelect.value
+    console.log(selectedCuisine)
+    cuisineForm.action =  `${selectedCity}/ ${selectedCuisine}`
+    console.log(cuisineForm.action)
 })
